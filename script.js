fd455ced3efc8f3228d9ab014370f14e9aa41207
SaerElMasri <saer1890@gmail.com>
2023-03-11 11:02:31 +0200

    Added register.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('#login').onclick = () => {
+    window.location.href = "http://localhost/health-website-frontend/pages/login.html";
+  }
---
+++ scripts/log.js
@@ -0,0 +1,54 @@
+//Register User
+document.querySelector('#register-action').onclick = () =>{
+    let id = document.getElementById('user_type')
+    let firstName = document.getElementById('first_name')
+    let lastName = document.getElementById('last_name')
+    let email = document.getElementById('email')
+    let dob = document.getElementById('dob')
+    let password = document.getElementById('password')
+    let repeatPassword = document.getElementById('confirm_password')
+    const data = new FormData();
+    data.append("email", email.value);
+    data.append("password", password.value);
+    data.append("first_name", firstName.value);
+    data.append("dob", dob.value);
+    data.append("usertype_id", id.value);
+    data.append("last_name", lastName.value);
+
+
+    if(id.value == "" && firstName.value == "" && lastName.value == "" && email.value == "" && dob.value == "" && password.value == ""){
+        alert("All Fields Required")
+    }else{
+        if(password.value !== repeatPassword.value){
+            alert("Passwords do not match");
+        }else{
+            if(isValidEmail(email)){
+                if(isValidPassword(password)){
+                    axios.post("http://localhost/health-website-backend/register.php", data).then((res) => {
+                    console.log(res);
+                    if (res.data.result == "Success") {
+                        alert("Signed up successfully!");
+                    }else{
+                        alert("Email Already taken");
+                    }
+                    }).catch((error) => {
+                        console.log(error);
+                    })
+                }else{
+                    alert("Password not valid")
+                }
+            }else{
+                alert("Email not valid");
+            }
+        }
+    }
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    const isValidPassword = (password) => {
+        const emailRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return emailRegex.test(password);
+    }
+}
+
