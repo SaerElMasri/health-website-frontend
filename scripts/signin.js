f3a0698a307804b7357e8404da84c33cd512837c
SaerElMasri <saer1890@gmail.com>
2023-03-11 13:06:57 +0200

    Fixed bugs in signin.js

--- scripts/signin.js
+++ scripts/signin.js
@@ -10,9 +10,11 @@ document.querySelector('#login-action').onclick = () => {
     }else{
         axios.post("http://localhost/health-website-backend/login.php", data)
           .then((res) => {
-            console.log(res.data);
+            console.log(res);
+            const id = res.data.user_id;
+            const user_type = res.data.usertype_id;
             if (res.data.response == "logged in") {
-              alert('Login In Successfully');
+              user_checker(user_type, id);
             } else {
               alert(res.data.response);
             }
@@ -25,4 +27,18 @@ document.querySelector('#login-action').onclick = () => {
 
 document.querySelector('#register').onclick = () => {
     window.location.href = "http://localhost/health-website-frontend/pages/registration.html";
+}
+
+const user_checker = (id, user_id) => {
+  if(id == 1){
+    //Client Site
+    window.location.href = `http://localhost/health-website-frontend/patient_site/pages/patient_dashboard.html?id=${user_id}`;
+  }else if(id == 2){
+    //Admin Site
+    window.location.href = "http://localhost/health-website-frontend/admin_site/pages/dashboard.html";
+  }else if(id == 3){
+    //Employee Site
+  }else{
+    alert("404");
+  }
 }
